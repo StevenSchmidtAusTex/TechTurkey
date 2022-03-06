98735764d5d593ce2ce9f86f57405836651295a6
Steven Schmidt <stevenschmidtaustex@gmail.com>
2022-03-05 18:00:59 -0600

    added obnoxious sound effects

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,10 +4,10 @@ const newFormHandler = async (event) => {
   const name = document.querySelector('#blog-name').value.trim();
   const description = document.querySelector('#blog-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && description) {
     const response = await fetch(`/api/blogs`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ name, description }),
       headers: {
         'Content-Type': 'application/json',
       },
